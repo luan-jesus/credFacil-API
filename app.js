82a80b9a683f4bfacaf39ca86791d24a1af8287d
Luan Jesus <luan.j0612@gmail.com>
2020-04-10 09:46:57 -0300

    port changed to 8081

--- app.js
+++ app.js
@@ -22,4 +22,4 @@ app.use("/", authController);
 app.use("/", userController);
 
 /** Server deployment **/
-app.listen(5000, () => console.log("Server running at port 5000"));
+app.listen(8081, () => console.log("Server running at port 8081"));
